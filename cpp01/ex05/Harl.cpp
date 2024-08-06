4955c414071fccfd8f1c89e4285a3a800994a5d6
Victoria Codreanu <vcodrean@c1r3s4.42madrid.com>
2024-08-06 13:31:13 +0200

    done cpp01

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -15,7 +15,7 @@ void Harl::debug(void)
 
 void Harl::info(void)
 {
-    std::cout << "vINFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void)
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -1,2 +1,32 @@
-#include "Hral.hpp"
+#include "Harl.hpp"
 
+
+int main()
+{
+	Harl harl;
+
+	std::cout << std::endl;
+
+	std::cout << "Testing DEBUG lavel: " << std::endl;
+	harl.complain("DEBUG");
+
+	std::cout << std::endl;
+
+	std::cout << "Testing INFO level:" << std::endl;
+	harl.complain("INFO");
+
+	std::cout << std::endl;
+
+	std::cout << "Testing WARNING level:" << std::endl;
+	harl.complain("WARNING");
+
+	std::cout << std::endl;
+
+	std::cout << "Testing ERROR level: " << std::endl;
+	harl.complain("ERROR");
+
+	std::cout << std::endl;
+
+
+
+}
