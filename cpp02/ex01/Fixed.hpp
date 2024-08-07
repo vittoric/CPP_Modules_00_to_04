3b7e7ac4525ba0eb8a060abe55352f45c48b130c
Victoria Codreanu <vcodrean@c1r1s6.42madrid.com>
2024-08-07 18:39:55 +0200

    in progress ex01

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -2,21 +2,30 @@
 #define FIXED_HPP
 #include <iostream>
 #include <cmath>
+#include <string>
 
 class Fixed
 {
-    private:
-        int _value;
-        static const int _fractionalBits = 8;
-    public:
-        Fixed();
-        Fixed(const Fixed &fixed);
-        Fixed &operator = (const Fixed &fixed);
-        ~Fixed();
+private:
+    int _value;
+    static const int _fractionalBits = 8;
 
-        float toFloat( void ) const;
-        int toInt( void ) const;
+public:
+    Fixed();
+    Fixed(const Fixed &fixed);
+    Fixed &operator=(const Fixed &fixed);
+    ~Fixed();
 
+    Fixed(int const intVal);
+    Fixed(float const floatVal);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+    float toFloat(void) const;
+    int toInt(void) const;
 };
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
 
 #endif
